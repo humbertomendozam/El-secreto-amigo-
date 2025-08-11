7a82da6dd22f257bd1d148d3210cb2d90b3f6f5f
humbertomendozam <humbertommanya@gmail.com>
2025-08-11 13:31:23 -0500

    Create App.js
    Sorpresa

---
+++ App.js
@@ -0,0 +1,5 @@
+function mostrarSorpresa() {
+  const sorpresa = document.getElementById("sorpresa");
+  sorpresa.innerHTML = "🎉 ¡Tu regalo está en camino! Gracias por ser parte de este juego secreto.";
+  sorpresa.classList.remove("oculto");
+}
